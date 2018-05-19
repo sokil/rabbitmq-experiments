e1a95fa0888ce62214f44f87b5667b2cbbb0e8ae
Dmytro Sokil <dmytro.sokil@gmail.com>
2018-05-19 09:15:27 +0300

    fix connection

--- Commands/FanoutCommand.php
+++ Commands/FanoutCommand.php
@@ -2,14 +2,27 @@
 
 namespace Command;
 
+use PhpAmqpLib\Connection\AbstractConnection;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class FanoutCommand extends Command
 {
+    /**
+     * @var AbstractConnection
+     */
+    private $amqpConnection;
+
+    public function __construct($name = null, AbstractConnection $amqpConnection)
+    {
+        parent::__construct($name);
+
+        $this->amqpConnection = $amqpConnection;
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-
+        $channel = $this->amqpConnection->channel();
     }
 }
--- run.php
+++ run.php
@@ -1,11 +1,32 @@
 #!/usr/bin/env php
 <?php
 
-require_once './vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\Dotenv\Dotenv;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Connection\AMQPLazySocketConnection;
 use Symfony\Component\Console\Application;
 
-$app = new Application();
-$app->add(new \Command\FanoutCommand("fanout"));
+// init configuration
 
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/docker/.env');
+
+// init dependencies
+$socketConnection = new AMQPLazySocketConnection(
+    'haproxy',
+    getenv('RABBITMQ_CLUSTER_PORT'),
+    getenv('RABBITMQ_DEFAULT_USER'),
+    getenv('RABBITMQ_DEFAULT_PASS')
+);
+
+// create console application
+$app = new Application();
+$app->add(new \Command\FanoutCommand(
+    "fanout",
+    $socketConnection
+));
+
+// run console application
 $app->run();
